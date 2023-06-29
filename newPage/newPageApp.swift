d6e96ab6cac919094bbac44b1b3b1e8eec2f5af1
Karmen <karmengeisel@icloud.com>
2023-06-29 10:01:04 -0600

    Initial Commit

---
+++ newPage/newPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  newPageApp.swift
+//  newPage
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct newPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
